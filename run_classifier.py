9235bdd4d897c5e01b4d4d691892369f99d51605
lee-thanhsang <sang.le15092001@hcmut.edu.vn>
2022-06-01 22:15:24 +0700

    first commit

---
+++ run_classifier.py
@@ -0,0 +1,23 @@
+ 
+from sentiment_reader import SentimentCorpus
+from multinomial_naive_bayes import MultinomialNaiveBayes
+
+if __name__ == '__main__':
+    dataset = SentimentCorpus()
+    nb = MultinomialNaiveBayes()
+    for i in range(20):
+        print(dataset.train_X[i])
+        print(dataset.train_y[i])
+    params = nb.train(dataset.train_X, dataset.train_y)
+    #loglikelihood of each word per class  row= no of words columns =classes
+    predict_train = nb.test(dataset.train_X, params)
+    #predict_train has the output of test doc belonging to which class
+    eval_train = nb.evaluate(predict_train, dataset.train_y)
+    
+    predict_test = nb.test(dataset.test_X, params)
+    eval_test = nb.evaluate(predict_test, dataset.test_y)
+    
+    print("Accuracy on training set: %f, on test set: %f" % (eval_train, eval_test))
+
+
+
